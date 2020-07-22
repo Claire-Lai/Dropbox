55b2288f302b376d77079110156c97831f37893d
lesleyc@bu.edu <lesleyc@bu.edu>
2020-07-22 12:05:18 +0800

    Adding Instructions

---
+++ index.js
@@ -0,0 +1,29 @@
+/**********************************************
+ * Dropbox Challenge
+ * ==================================
+ ***********************************************/
+
+// The purpose of this challenge is to ensure that you solidify your understanding of Node.js, as well as connect the frontend files to the backend.
+
+/** # SETUP #
+/*  ================== */
+
+/** 1) Packages are already installed. Import them to this file */
+
+/** # Create a Server #
+/*  ====================== */
+/** 2) Create a Server */
+/*
+Documentation: 
+- Example -
+*/
+
+/** # Create a Server #
+/*  ====================== */
+/** 2) Create a Server */
+/*
+Documentation: 
+
+
+- Example -
+*/
